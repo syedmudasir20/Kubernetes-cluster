829342c8b6a2ba618347cb9931d381f94b0cc1c7
syed mudasir <77150095+syedmudasir20@users.noreply.github.com>
2024-03-20 07:22:31 +0530

    Update k8s-pre-req.sh

--- k8s-pre-req.sh
+++ k8s-pre-req.sh
@@ -73,7 +73,7 @@ echo
 echo "Install kubeadm, kubelet, and kubectl"
 sleep 2
 
-sudo sh -c "echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.27/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list"
+sudo sh -c "echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.27/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list"
 
 sudo sh -c "curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.27/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg"
 
